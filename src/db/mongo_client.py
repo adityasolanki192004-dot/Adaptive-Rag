d882ad6b7cf06dd43ff0d0fb1049ff149420928a
ADITYA SOLANKI <adityasolanki192004@gmail.com>
2026-07-31 00:55:50 +0530

    Update mongo_client.py

--- src/db/mongo_client.py
+++ src/db/mongo_client.py
@@ -5,8 +5,8 @@ MongoDB client initialization.
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = "adaptive_rag"
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("MONGO_DB_NAME", "adaptive_rag")
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
