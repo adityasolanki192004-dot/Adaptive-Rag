e825e1e58d9bea48daa936e6683a848179e5af05
ADITYA SOLANKI <adityasolanki192004@gmail.com>
2026-07-30 14:30:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,29 @@ question = st.text_input("Ask a question")
 
 # Send Button
 if uploaded_file:
-    st.success("Document Uploaded Successfully!")
+
+    files = {
+        "file": (
+            uploaded_file.name,
+            uploaded_file.getvalue(),
+            uploaded_file.type
+        )
+    }
+
+    headers = {
+        "X-Description": uploaded_file.name
+    }
+
+    upload_response = requests.post(
+        f"{BACKEND_URL}/rag/documents/upload",
+        files=files,
+        headers=headers
+    )
+
+    if upload_response.status_code == 200:
+        st.success("Document Uploaded Successfully!")
+    else:
+        st.error(upload_response.text)
 if st.button("Send"):
     if question.strip():
         try:
